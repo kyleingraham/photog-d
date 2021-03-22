b23d2797a03a12f57abd127a85809e25fccc5743
Kyle Ingraham <kyle@kyleingraham.com>
2021-03-21 22:43:03 -0400

    Add utils Module and Refactor Documentation

--- source/photog/color.d
+++ source/photog/color.d
@@ -3,6 +3,9 @@ module photog.color;
 import ldc.attributes : fastmath;
 import mir.ndslice : Slice, sliced, SliceKind;
 
+import photog.utils;
+
+// dfmt off
 /**
 RGB working spaces.
 */
@@ -16,7 +19,7 @@ Color space conversion matrices.
 */
 private static immutable
 {
-    // dfmt off
+    
     auto sRgbRgb2Xyz = [
         0.4124564, 0.3575761, 0.1804375,
         0.2126729, 0.7151522, 0.0721750,
@@ -34,22 +37,48 @@ private static immutable
 		-0.9692660, 1.8760108, 0.0415560,
 		0.0556434, -0.2040259, 1.0572252
     ];
-    // dfmt on
 }
 
 /**
-Convert RGB input to XYZ.
-
-Params:
-	workingSpace = working space of input image. Determines reference white. Defaults to sRGB (D65).
-	ReturnType = element type for return slice. Determines output buffer type. Must be floating point. Defaults to double.
-	input = Input image with 3 channels represented as R, G, and B respectively in that order.
-	output = buffer where converted image will be copied. Reallocated if shape does not match input. Defaults to empty slice.
-
-Returns:
-	Returns XYZ version of the RGB input image.
+Standard illuminants.
 */
-Slice!(ReturnType*, 3) rgb2Xyz(WorkingSpace workingSpace = WorkingSpace.sRgb, ReturnType = double, T)(Slice!(T, 3) input)
+enum Illuminant
+{
+    d65 = [0.95047, 1.0, 1.08883]
+}
+
+/**
+Chromatic adaptation methods.
+*/
+enum ChromAdaptMethod
+{
+    bradford = "bradford"
+}
+
+/**
+Chromatic adaptation method matrices.
+*/
+private static immutable
+{
+    auto bradfordXyz2Lms = [
+        0.8951, 0.2664, -0.1614,
+        -0.7502, 1.7135, 0.0367,
+        0.0389, -0.0685, 1.0296
+    ];
+    
+    auto bradfordInvXyz2Lms = [
+        0.9869929, -0.1470543, 0.1599627,
+        0.4323053, 0.5183603, 0.0492912,
+        -0.0085287, 0.0400428, 0.9684867
+    ];
+}
+// dfmt on
+
+/**
+Convert RGB input to XYZ.
+*/
+Slice!(ReturnType*, 3) rgb2Xyz(WorkingSpace workingSpace = WorkingSpace.sRgb, ReturnType = double,
+        T)(Slice!(T, 3) input)
 {
     return rgbBgr2Xyz!(false, workingSpace, ReturnType)(input);
 }
@@ -79,17 +108,9 @@ unittest
 
 /**
 Convert BGR input to XYZ.
-
-Params:
-	workingSpace = working space of input image. Determines reference white. Defaults to sRGB (D65).
-	ReturnType = element type for return slice. Determines output buffer type. Must be floating point. Defaults to double.
-	input = Input image with 3 channels represented as B, G, and R respectively in that order.
-	output = buffer where converted image will be copied. Reallocated if shape does not match input. Defaults to empty slice.
-
-Returns:
-	Returns XYZ version of the BGR input image.
 */
-Slice!(ReturnType*, 3) bgr2Xyz(WorkingSpace workingSpace = WorkingSpace.sRgb, ReturnType = double, T)(Slice!(T, 3) input)
+Slice!(ReturnType*, 3) bgr2Xyz(WorkingSpace workingSpace = WorkingSpace.sRgb, ReturnType = double,
+        T)(Slice!(T, 3) input)
 {
     return rgbBgr2Xyz!(true, workingSpace, ReturnType)(input);
 }
@@ -117,7 +138,8 @@ unittest
     assert(approxEqual(bgr.bgr2Xyz, xyz));
 }
 
-private Slice!(ReturnType*, 3) rgbBgr2Xyz(bool isBgr, WorkingSpace workingSpace, ReturnType, T)(Slice!(T, 3) input)
+private Slice!(ReturnType*, 3) rgbBgr2Xyz(bool isBgr, WorkingSpace workingSpace, ReturnType, T)(
+        Slice!(T, 3) input)
 in
 {
     import std.traits : isFloatingPoint;
@@ -139,25 +161,26 @@ do
     else
         auto conversionMatrix = mixin(workingSpace ~ "Rgb2Xyz").sliced(3, 3);
 
-    pixelZip.each!((z) {z.rgbBgr2XyzImpl!(isBgr, workingSpace)(conversionMatrix);});
+    pixelZip.each!((z) {
+        z.rgbBgr2XyzImpl!(isBgr, workingSpace)(conversionMatrix);
+    });
 
     return output;
 }
 
-@fastmath
-void rgbBgr2XyzImpl(bool isBgr, WorkingSpace workingSpace, T, U)(T pixelZip, Slice!(U, 2) conversionMatrix)
+@fastmath void rgbBgr2XyzImpl(bool isBgr, WorkingSpace workingSpace, T, U)(
+        T pixelZip, Slice!(U, 2) conversionMatrix)
 {
     import kaleidic.lubeck : mtimes;
     import mir.ndslice : each;
-    
+
     // pixelZip[0] = input
     // pixelZip[1] = output
 
     auto output = sliced(pixelZip[0].ptr, 3);
 
     // Linearize
-    output.each!((ref chnl)
-    {
+    output.each!((ref chnl) {
         if (chnl <= 0.04045)
             chnl = chnl / 12.92;
         else
@@ -170,7 +193,11 @@ void rgbBgr2XyzImpl(bool isBgr, WorkingSpace workingSpace, T, U)(T pixelZip, Sli
     pixelZip[1][] = output.field;
 }
 
-Slice!(ReturnType*, 3) xyz2Rgb(WorkingSpace workingSpace = WorkingSpace.sRgb, ReturnType = double, T)(Slice!(T, 3) input)
+/**
+Convert XYZ input to RGB.
+*/
+Slice!(ReturnType*, 3) xyz2Rgb(WorkingSpace workingSpace = WorkingSpace.sRgb, ReturnType = double,
+        T)(Slice!(T, 3) input)
 {
     return xyz2RgbBgr!(false, workingSpace, ReturnType)(input);
 }
@@ -198,7 +225,11 @@ unittest
     assert(approxEqual(xyz.xyz2Rgb, rgb, 0.01, 1e-04));
 }
 
-Slice!(ReturnType*, 3) xyz2Bgr(WorkingSpace workingSpace = WorkingSpace.sRgb, ReturnType = double, T)(Slice!(T, 3) input)
+/**
+Convert XYZ input to BGR.
+*/
+Slice!(ReturnType*, 3) xyz2Bgr(WorkingSpace workingSpace = WorkingSpace.sRgb, ReturnType = double,
+        T)(Slice!(T, 3) input)
 {
     return xyz2RgbBgr!(true, workingSpace, ReturnType)(input);
 }
@@ -226,100 +257,36 @@ unittest
     assert(approxEqual(xyz.xyz2Bgr, bgr, 0.01, 1e-04));
 }
 
-/**
-Convert [M, N, P] slice to [MxN] slice of [P] arrays.
-*/
-auto pixelPack(size_t chnls, InputType, size_t dims, SliceKind kind)(Slice!(InputType*, dims, kind) image)
-in
-{
-    assert(dims == 3, "Image must have 3 dimensions.");
-}
-do
-{
-    // TODO: How to we specify strides? e.g. Slice!(ReturnType*, 1, kind)(shape, strides, iterator);
-    size_t[1] shape = [image.shape[0] * image.shape[1]];
-    alias ReturnType = InputType[chnls];
-    ReturnType* iterator = cast(ReturnType*) image.iterator;
-    return Slice!(ReturnType*, 1, kind)(shape, iterator);
-}
-
-unittest
-{
-    // dfmt off
-    Slice!(double*, 3) rgb = [
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0,
-        0.470588, 0.470588, 0.470588
-    ].sliced(4, 1, 3);
-    // dfmt on
-
-    auto packed = pixelPack!3(rgb);
-    assert(packed.shape == [4 * 1]);
-    assert(packed[0].length == 3);
-}
-
-/**
-Convert [MxN] slice of [P] arrays to [M, N, P] slice.
-*/
-auto pixelUnpack(size_t chnls, InputType, size_t dims, SliceKind kind)(Slice!(InputType*, dims, kind) image, size_t height, size_t width)
-in
-{
-    // TODO: Validate chnls * width * height == # of elements in array underlying image slice.
-    assert(dims == 1, "Image must have 1 dimension.");
-}
-do
-{
-    size_t[3] shape = [height, width, chnls];
-    alias ReturnType = IteratorType!InputType;
-    ReturnType* iterator = cast(ReturnType*) image.iterator;
-    return Slice!(ReturnType*, 3, kind)(shape, iterator);
-}
-
-unittest
-{
-    // dfmt off
-    Slice!(double[]*, 1) rgb = [
-        [1.0, 0.0, 0.0],
-        [0.0, 1.0, 0.0],
-        [0.0, 0.0, 1.0],
-        [0.470588, 0.470588, 0.470588]
-    ].sliced(4);
-    // dfmt on
-
-    auto unpacked = pixelUnpack!3(rgb, 4, 1);
-    assert(unpacked.shape == [4, 1, 3]);
-    assert(unpacked[0][0].length == 3);
-}
-
-private Slice!(ReturnType*, 3) xyz2RgbBgr(bool isBgr, WorkingSpace workingSpace, ReturnType, T)(Slice!(T, 3) input)
+private Slice!(ReturnType*, 3) xyz2RgbBgr(bool isBgr, WorkingSpace workingSpace, ReturnType, T)(
+        Slice!(T, 3) input)
 in
 {
     import std.traits : isFloatingPoint;
 
     static assert(isFloatingPoint!ReturnType, "Return type must be floating point.");
-    assert(input.shape[2] == 3, "Input requires 3 channels.");
 }
 do
 {
-	import mir.ndslice : each, uninitSlice, zip;
+    import mir.ndslice : each, uninitSlice, zip;
 
     auto output = uninitSlice!ReturnType(input.shape);
     auto inputPack = pixelPack!3(input);
     auto outputPack = pixelPack!3(output);
     auto pixelZip = zip(inputPack, outputPack);
     auto conversionMatrix = mixin(workingSpace ~ "Xyz2Rgb").sliced(3, 3); // TODO: Known at compile-time. Send as template?
-    pixelZip.each!((z) {z.xyz2RgbBgrImpl!(isBgr, workingSpace)(conversionMatrix);});
+    pixelZip.each!((z) {
+        z.xyz2RgbBgrImpl!(isBgr, workingSpace)(conversionMatrix);
+    });
     // No need to unpack output. Underlying data has already been changed.
     return output;
 }
 
-@fastmath
-void xyz2RgbBgrImpl(bool isBgr, WorkingSpace workingSpace, T, U)(T pixelZip, Slice!(U, 2) conversionMatrix)
+@fastmath void xyz2RgbBgrImpl(bool isBgr, WorkingSpace workingSpace, T, U)(
+        T pixelZip, Slice!(U, 2) conversionMatrix)
 {
-	import kaleidic.lubeck : mtimes;
-	import mir.ndslice : each;
-	
+    import kaleidic.lubeck : mtimes;
+    import mir.ndslice : each;
+
     // pixelZip[0] = input
     // pixelZip[1] = output
 
@@ -328,8 +295,7 @@ void xyz2RgbBgrImpl(bool isBgr, WorkingSpace workingSpace, T, U)(T pixelZip, Sli
     auto output = conversionMatrix.mtimes(sliced(pixelZip[0].ptr, 3));
 
     // Un-linearize
-    output.each!((ref chnl)
-    {
+    output.each!((ref chnl) {
         if (chnl <= 0.0031308)
             chnl = chnl * 12.92;
         else
@@ -347,275 +313,18 @@ void xyz2RgbBgrImpl(bool isBgr, WorkingSpace workingSpace, T, U)(T pixelZip, Sli
 }
 
 /**
-Clip the input to the range provided.
+Chromatically adapt RGB input from the given source illuminant to the 
+given destination illuminant.
 */
-T clip(double low, double high, T)(T value)
-in
-{
-	import std.traits : isFloatingPoint;
-	
-	static assert(isFloatingPoint!T, "Value to clip must be floating point.");
-	static assert(low >= -T.max);
-	static assert(high <= T.max);
-}
-do
-{
-	if (value >= high)
-		return high;
-	else if (value <= low)
-		return low;
-	else
-		return value;
-}
-
-/**
-Grabs iterator type at compile-time.
-*/
-private template IteratorType(Iterator)
-{
-	import std.traits : Unqual;
-	
-	alias T = Unqual!(typeof(Iterator.init[0]));
-	alias IteratorType = T;
-}
-
-unittest
-{
-	import mir.ndslice : slice;
-	
-	alias ExpectedType = long;
-	
-	void testIteratorType(Iterator)(Slice!(Iterator, 3) testSlice)
-	{
-		assert(is(IteratorType!Iterator == ExpectedType));
-	}
-	
-	auto a = slice!(immutable(ExpectedType))([1, 2, 3], 0);
-	testIteratorType(a);
-}
-
-/**
-Recursively determine dimensions of a Slice.
-*/
-size_t[] dimensions(T)(T arr, size_t[] dims = []) {
-	import std.traits : isNumeric;
-	
-	static if (isNumeric!T)
-		return dims;
-	else
-	{
-		dims ~= arr.length;
-		return dimensions(arr[0], dims);
-	}
-}
-
-unittest
-{
-	import mir.ndslice : slice;
-
-	size_t[3] expectedDims = [1, 2, 3];
-	auto a = slice!double(expectedDims, 0);
-	assert(a.dimensions == expectedDims);
-}
-
-/**
-Calculates the mean pixel value for an image.
-
-Return semantics match mir.math.stat.mean.
-*/
-auto imageMean(Iterator)(Slice!(Iterator, 3) image)
-in
-{
-    assert(image.shape[2] == 3, "Input requires 3 channels.");
-}
-do
-{
-    import mir.math.stat : mean;
-    import mir.ndslice : byDim, map, slice;
-
-    // TODO: Add option for segmenting image for calculating mean.
-    // TODO: Add option to exclude top and bottom percentiles from mean.
-    // dfmt off
-    return image
-        .byDim!2
-        .map!(mean)
-        .slice;
-    //dfmt on
-}
-
-unittest
-{
-    // dfmt off
-    ubyte[] rgb = [
-        255, 0, 0,
-        0, 255, 0,
-        0, 0, 255,
-        120, 120, 120
-    ];
-    //dfmt on
-
-    assert(rgb.sliced(4, 1, 3).imageMean == [93.75, 93.75, 93.75]);
-}
-
-Slice!(ReturnType*, dims) toUnsigned(ReturnType = ubyte, InputType, size_t dims)(Slice!(InputType*, dims) image)
-in
-{
-    import std.traits : isFloatingPoint, isUnsigned;
-
-    static assert(isFloatingPoint!InputType);
-    static assert(isUnsigned!ReturnType);
-}
-do
-{
-    // TODO: Handle being passed an unsigned slice.
-    import mir.ndslice : each, uninitSlice, zip;
-
-    auto output = uninitSlice!ReturnType(image.shape);
-    auto zipped = zip(image, output);
-    zipped.each!((z) { toUnsignedImpl(z); });
-
-    return output;
-}
-
-@fastmath
-void toUnsignedImpl(T)(T zippedChnls)
-{
-    import std.math : round;
-    
-    alias UnsignedType = typeof(zippedChnls[1].__value());
-    zippedChnls[1].__value() = cast(UnsignedType) round(zippedChnls[0].__value().clip!(0, 1) * UnsignedType.max);
-}
-
-unittest
-{
-    import std.math : approxEqual;
-    import mir.ndslice : sliced;
-
-    // dfmt off
-    Slice!(double*, 3) rgb = [
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0,
-        0.470588, 0.470588, 0.470588
-    ].sliced(4, 1, 3);
-
-    ubyte[] rgbU = [
-        255, 0, 0,
-        0, 255, 0,
-        0, 0, 255,
-        120, 120, 120
-    ];
-    Slice!(ubyte*, 3) rgbUnsigned = rgbU.sliced(4, 1, 3);
-    //dfmt on
-
-    assert(approxEqual(rgb.toUnsigned, rgbUnsigned));
-}
-
-Slice!(ReturnType*, 3) toFloating(ReturnType = double, InputType)(InputType[] image, uint width, uint height)
-in
-{
-    import std.traits : isFloatingPoint, isUnsigned;
-
-    static assert(isFloatingPoint!ReturnType);
-    static assert(isUnsigned!InputType);
-}
-do
-{
-    import mir.ndslice : each, uninitSlice, zip;
-
-    auto output = uninitSlice!ReturnType([height, width, 3]);
-    auto zipped = zip(image.sliced(height, width, 3), output);
-    zipped.each!((z) { toFloatingImpl(z); });
-
-    return output;
-}
-
-@fastmath
-private void toFloatingImpl(T)(T zippedChnls)
-{
-    alias UnsignedType = typeof(zippedChnls[0].__value());
-    alias FloatingType = typeof(zippedChnls[1].__value());
-    zippedChnls[1].__value() = cast(FloatingType) zippedChnls[0] / UnsignedType.max;
-}
-
-unittest
-{
-    import std.math : approxEqual;
-
-    // dfmt off
-    ubyte[] rgb = [
-        255, 0, 0,
-        0, 255, 0,
-        0, 0, 255,
-        120, 120, 120
-    ];
-
-    Slice!(double*, 3) rgbDouble = [
-        1.0, 0.0, 0.0,
-        0.0, 1.0, 0.0,
-        0.0, 0.0, 1.0,
-        0.470588, 0.470588, 0.470588
-    ].sliced(4, 1, 3);
-    //dfmt on
-
-    assert(approxEqual(rgb.toFloating(1, 4), rgbDouble));
-}
-
-/**
-Standard illuminants.
-*/
-enum Illuminant
-{
-    d65 = [0.95047, 1.0, 1.08883]
-}
-
-enum ChromAdaptMethod
-{
-    bradford = "bradford"
-}
-
-private static immutable
-{
-    // dfmt off
-    auto bradfordXyz2Lms = [
-        0.8951, 0.2664, -0.1614,
-        -0.7502, 1.7135, 0.0367,
-        0.0389, -0.0685, 1.0296
-    ];
-    
-    auto bradfordInvXyz2Lms = [
-        0.9869929, -0.1470543, 0.1599627,
-        0.4323053, 0.5183603, 0.0492912,
-        -0.0085287, 0.0400428, 0.9684867
-    ];
-    // dfmt on
-}
-
-auto pixelMap(alias fun, Iterator)(Slice!(Iterator, 3) image)
-{
-    import mir.ndslice : fuse, map, pack;
-
-    return image
-        .pack!1
-        .map!(fun)
-        .fuse;
-}
-
-/**
-Inputs:
-    RGB source image (double)
-    XYZ white point of source image under source illuminant -> array to allow non-standard illuminants
-    XYZ white point of destination illuminant -> array to support wide range of possible source illuminants
-    working space
-
-    is it pure if I pass in arrays?
-*/
-Slice!(Iterator, 3) chromAdapt(ChromAdaptMethod method = ChromAdaptMethod.bradford, Iterator)(Slice!(Iterator, 3) image, double[] srcIlluminant, double[] destIlluminant, WorkingSpace workingSpace = WorkingSpace.sRgb)
+Slice!(Iterator, 3) chromAdapt(ChromAdaptMethod method = ChromAdaptMethod.bradford, Iterator)(
+        Slice!(Iterator, 3) input,
+        double[] srcIlluminant, double[] destIlluminant,
+        WorkingSpace workingSpace = WorkingSpace.sRgb)
 in
 {
     import std.traits : isFloatingPoint;
-    
-    static assert(isFloatingPoint!(IteratorType!Iterator), "Image values must be floating point.");
+
+    static assert(isFloatingPoint!(IteratorType!Iterator), "Input values must be floating point.");
     assert(srcIlluminant.length == 3);
     assert(destIlluminant.length == 3);
 }
@@ -627,31 +336,19 @@ do
     auto xyz2Lms = mixin(method ~ "Xyz2Lms");
     auto lms2Xyz = mixin(method ~ "InvXyz2Lms");
 
-    auto lmsSrc = xyz2Lms
-        .sliced(3, 3)
-        .mtimes(srcIlluminant.sliced(3, 1));
-    
+    auto lmsSrc = xyz2Lms.sliced(3, 3).mtimes(srcIlluminant.sliced(3, 1));
 
-    auto lmsDest = xyz2Lms
-        .sliced(3, 3)
-        .mtimes(destIlluminant.sliced(3, 1));
-    
+    auto lmsDest = xyz2Lms.sliced(3, 3).mtimes(destIlluminant.sliced(3, 1));
 
     int err;
-    auto lmsGain = slice!double([3, 3], 0); 
+    auto lmsGain = slice!double([3, 3], 0);
     auto diag = lmsGain.diagonal;
     diag[] = (lmsDest / lmsSrc).reshape([3], err);
     assert(err == 0);
 
-    auto transform = lms2Xyz
-        .sliced(3, 3)
-        .mtimes(lmsGain)
-        .mtimes(xyz2Lms.sliced(3, 3));
+    auto transform = lms2Xyz.sliced(3, 3).mtimes(lmsGain).mtimes(xyz2Lms.sliced(3, 3));
 
-    return image
-        .rgb2Xyz
-        .pixelMap!(pixel => transform.mtimes(pixel))
-        .xyz2Rgb;
+    return input.rgb2Xyz.pixelMap!(pixel => transform.mtimes(pixel)).xyz2Rgb;
 }
 
 unittest
@@ -676,7 +373,7 @@ unittest
         .field;
     assert(err == 0);
     //dfmt on
-    
+
     auto ca = chromAdapt(image, srcIlluminant, Illuminant.d65);
     // TODO: Add assertion on chromAdapt output.
 }
---
+++ source/photog/utils.d
@@ -0,0 +1,302 @@
+module photog.utils;
+
+import ldc.attributes : fastmath;
+import mir.ndslice : Slice, sliced, SliceKind;
+
+/**
+Convert [M, N, P] slice to [MxN] slice of [P] arrays.
+*/
+auto pixelPack(size_t chnls, InputType, size_t dims, SliceKind kind)(
+        Slice!(InputType*, dims, kind) image)
+in
+{
+    assert(dims == 3, "Image must have 3 dimensions.");
+}
+do
+{
+    // TODO: How to we specify strides? e.g. Slice!(ReturnType*, 1, kind)(shape, strides, iterator);
+    size_t[1] shape = [image.shape[0] * image.shape[1]];
+    alias ReturnType = InputType[chnls];
+    ReturnType* iterator = cast(ReturnType*) image.iterator;
+    return Slice!(ReturnType*, 1, kind)(shape, iterator);
+}
+
+unittest
+{
+    // dfmt off
+    Slice!(double*, 3) rgb = [
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        0.470588, 0.470588, 0.470588
+    ].sliced(4, 1, 3);
+    // dfmt on
+
+    auto packed = pixelPack!3(rgb);
+    assert(packed.shape == [4 * 1]);
+    assert(packed[0].length == 3);
+}
+
+/**
+Convert [MxN] slice of [P] arrays to [M, N, P] slice.
+*/
+auto pixelUnpack(size_t chnls, InputType, size_t dims, SliceKind kind)(
+        Slice!(InputType*, dims, kind) image, size_t height, size_t width)
+in
+{
+    // TODO: Validate chnls * width * height == # of elements in array underlying image slice.
+    assert(dims == 1, "Image must have 1 dimension.");
+}
+do
+{
+    size_t[3] shape = [height, width, chnls];
+    alias ReturnType = IteratorType!InputType;
+    ReturnType* iterator = cast(ReturnType*) image.iterator;
+    return Slice!(ReturnType*, 3, kind)(shape, iterator);
+}
+
+unittest
+{
+    // dfmt off
+    Slice!(double[]*, 1) rgb = [
+        [1.0, 0.0, 0.0],
+        [0.0, 1.0, 0.0],
+        [0.0, 0.0, 1.0],
+        [0.470588, 0.470588, 0.470588]
+    ].sliced(4);
+    // dfmt on
+
+    auto unpacked = pixelUnpack!3(rgb, 4, 1);
+    assert(unpacked.shape == [4, 1, 3]);
+    assert(unpacked[0][0].length == 3);
+}
+
+/**
+Clip value to the range provided.
+*/
+T clip(double low, double high, T)(T value)
+in
+{
+    import std.traits : isFloatingPoint;
+
+    static assert(isFloatingPoint!T, "Value to clip must be floating point.");
+    static assert(low >= -T.max);
+    static assert(high <= T.max);
+}
+do
+{
+    if (value >= high)
+        return high;
+    else if (value <= low)
+        return low;
+    else
+        return value;
+}
+
+/**
+Grabs iterator type at compile-time.
+*/
+package template IteratorType(Iterator)
+{
+    import std.traits : Unqual;
+
+    alias T = Unqual!(typeof(Iterator.init[0]));
+    alias IteratorType = T;
+}
+
+unittest
+{
+    import mir.ndslice : slice;
+
+    alias ExpectedType = long;
+
+    void testIteratorType(Iterator)(Slice!(Iterator, 3) testSlice)
+    {
+        assert(is(IteratorType!Iterator == ExpectedType));
+    }
+
+    auto a = slice!(immutable(ExpectedType))([1, 2, 3], 0);
+    testIteratorType(a);
+}
+
+/**
+Recursively determine dimensions of a Slice.
+*/
+size_t[] dimensions(T)(T arr, size_t[] dims = [])
+{
+    import std.traits : isNumeric;
+
+    static if (isNumeric!T)
+        return dims;
+    else
+    {
+        dims ~= arr.length;
+        return dimensions(arr[0], dims);
+    }
+}
+
+unittest
+{
+    import mir.ndslice : slice;
+
+    size_t[3] expectedDims = [1, 2, 3];
+    auto a = slice!double(expectedDims, 0);
+    assert(a.dimensions == expectedDims);
+}
+
+/**
+Convert floating point input to unsigned.
+*/
+Slice!(ReturnType*, dims) toUnsigned(ReturnType = ubyte, InputType, size_t dims)(
+        Slice!(InputType*, dims) input)
+in
+{
+    import std.traits : isFloatingPoint, isUnsigned;
+
+    static assert(isFloatingPoint!InputType);
+    static assert(isUnsigned!ReturnType);
+}
+do
+{
+    // TODO: Handle being passed an unsigned slice.
+    import mir.ndslice : each, uninitSlice, zip;
+
+    auto output = uninitSlice!ReturnType(input.shape);
+    auto zipped = zip(input, output);
+    zipped.each!((z) { toUnsignedImpl(z); });
+
+    return output;
+}
+
+@fastmath private void toUnsignedImpl(T)(T zippedChnls)
+{
+    import std.math : round;
+
+    alias UnsignedType = typeof(zippedChnls[1].__value());
+    zippedChnls[1].__value() = cast(UnsignedType) round(zippedChnls[0].__value()
+            .clip!(0, 1) * UnsignedType.max);
+}
+
+unittest
+{
+    import std.math : approxEqual;
+    import mir.ndslice : sliced;
+
+    // dfmt off
+    Slice!(double*, 3) rgb = [
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        0.470588, 0.470588, 0.470588
+    ].sliced(4, 1, 3);
+
+    ubyte[] rgbU = [
+        255, 0, 0,
+        0, 255, 0,
+        0, 0, 255,
+        120, 120, 120
+    ];
+    Slice!(ubyte*, 3) rgbUnsigned = rgbU.sliced(4, 1, 3);
+    //dfmt on
+
+    assert(approxEqual(rgb.toUnsigned, rgbUnsigned));
+}
+
+/**
+Convert unsigned input to floating point.
+*/
+Slice!(ReturnType*, 3) toFloating(ReturnType = double, InputType)(InputType[] input,
+        uint width, uint height)
+in
+{
+    import std.traits : isFloatingPoint, isUnsigned;
+
+    static assert(isFloatingPoint!ReturnType);
+    static assert(isUnsigned!InputType);
+}
+do
+{
+    import mir.ndslice : each, uninitSlice, zip;
+
+    auto output = uninitSlice!ReturnType([height, width, 3]);
+    auto zipped = zip(input.sliced(height, width, 3), output);
+    zipped.each!((z) { toFloatingImpl(z); });
+
+    return output;
+}
+
+@fastmath private void toFloatingImpl(T)(T zippedChnls)
+{
+    alias UnsignedType = typeof(zippedChnls[0].__value());
+    alias FloatingType = typeof(zippedChnls[1].__value());
+    zippedChnls[1].__value() = cast(FloatingType) zippedChnls[0] / UnsignedType.max;
+}
+
+unittest
+{
+    import std.math : approxEqual;
+
+    // dfmt off
+    ubyte[] rgb = [
+        255, 0, 0,
+        0, 255, 0,
+        0, 0, 255,
+        120, 120, 120
+    ];
+
+    Slice!(double*, 3) rgbDouble = [
+        1.0, 0.0, 0.0,
+        0.0, 1.0, 0.0,
+        0.0, 0.0, 1.0,
+        0.470588, 0.470588, 0.470588
+    ].sliced(4, 1, 3);
+    //dfmt on
+
+    assert(approxEqual(rgb.toFloating(1, 4), rgbDouble));
+}
+
+/**
+Calculate the mean pixel value for an image.
+
+Return semantics match mir.math.stat.mean.
+*/
+auto imageMean(T)(Slice!(T, 3) image)
+{
+    import mir.math.stat : mean;
+    import mir.ndslice : byDim, map, slice;
+
+    // TODO: Add option for segmenting image for calculating mean.
+    // TODO: Add option to exclude top and bottom percentiles from mean.
+    // dfmt off
+    return image
+        .byDim!2
+        .map!(mean)
+        .slice;
+    //dfmt on
+}
+
+unittest
+{
+    // dfmt off
+    ubyte[] rgb = [
+        255, 0, 0,
+        0, 255, 0,
+        0, 0, 255,
+        120, 120, 120
+    ];
+    //dfmt on
+
+    assert(rgb.sliced(4, 1, 3).imageMean == [93.75, 93.75, 93.75]);
+}
+
+/**
+Map a function across an image's pixels.
+*/
+auto pixelMap(alias fun, Iterator)(Slice!(Iterator, 3) image)
+{
+    import mir.ndslice : fuse, map, pack;
+
+    return image.pack!1
+        .map!(fun)
+        .fuse;
+}
